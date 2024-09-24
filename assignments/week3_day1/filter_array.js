c4080a1a7067f4a8712188bb2aa954744243644b
omotomiwa26 <omotomiwa26@gmail.com>
2024-09-24 13:59:07 +0100

    added filter array file

---
+++ assignments/week3_day1/filter_array.js
@@ -0,0 +1,6 @@
+// Use filter() to create a new array from [10, 15, 20, 25, 30] that only contains numbers greater than 20.
+
+const numbers = [10, 15, 20, 25, 30];
+const numbers_greater_than_20 = numbers.filter(number => number > 20);
+console.log(numbers_greater_than_20);
+

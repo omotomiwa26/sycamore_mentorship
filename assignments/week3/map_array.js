3cffcc90440ebbb0fbfdcf9ea946323c499e1a4c
omotomiwa26 <omotomiwa26@gmail.com>
2024-09-24 13:40:12 +0100

    deleted files

--- assignments/week3/map_array.js
+++
@@ -1,7 +0,0 @@
-// Use map() to create a new array where each number from the array [1, 2, 3, 4, 5] is multiplied by 3.
-
-const numbers = [1, 2, 3, 4, 5];
-const tripled_numbers = numbers.map(function(number) {
-  return number * 3;
-});
-console.log(tripled_numbers);
--- assignments/week3/readme.md
+++
@@ -1,1 +0,0 @@
-## This directory contains week3 assingment files for sycamore mentorship program

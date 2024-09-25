f20a1c1a2ef3a70423ebff94d286a595769ad72a
omotomiwa26 <omotomiwa26@gmail.com>
2024-09-25 15:35:50 +0100

    updated fetching weather with error handling

--- assignments/week3_day2/fetch_weather.js
+++ assignments/week3_day2/fetch_weather.js
@@ -52,3 +52,36 @@ fetchWeather()
 
   getWeather();
 
+//---------------------------------------------------------------------------------------
+
+//Handle errors by rejecting the promise when the city is not "Lagos" and using try...catch to catch the error.
+
+const fetchWeather3 = (city) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (city === 'Lagos') {
+        resolve({
+          location: 'Lagos, Nigeria',
+          temperature: -5,
+          weather: 'Snowy'
+        });
+      } else {
+        reject('Cant Fetch City');
+      }
+    }, 2000);
+  });
+}
+
+const getWeather2 = async () => {
+  try {
+    const data = await fetchWeather3('Lagos');
+    console.log(`Current Location is: ${data.location}`);
+    console.log(`Current Temperature in ${data.location} is: ${data.temperature}°C`);
+    console.log(`Current Weather Status in ${data.location} is: ${data.weather}`);
+  } catch (error) {
+    console.log(error);
+  }
+} 
+
+getWeather2();
+

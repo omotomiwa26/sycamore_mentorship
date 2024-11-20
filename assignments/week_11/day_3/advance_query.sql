2ce5ccc397556ebbca51a73bff8503987829b1e4
omotomiwa26 <omotomiwa26@gmail.com>
2024-11-20 15:26:40 +0100

    left join

--- assignments/week_11/day_3/advance_query.sql
+++ assignments/week_11/day_3/advance_query.sql
@@ -10,6 +10,13 @@ on s.book_id = b.id
 --Exercise 2: Using LEFT JOIN
 --Write a query to retrieve all authors and their books. Include authors who haven’t written any books.
 
+select a.name, b.title
+from
+authors a
+left join
+books b
+on a.id = b.author_id
+
 --Exercise 3: Using RIGHT JOIN
 --Write a query to retrieve all books and their corresponding authors. Include books without a known author.
 

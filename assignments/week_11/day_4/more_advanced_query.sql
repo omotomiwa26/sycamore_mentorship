6e80650e89e05c5c9240e2f2d2afc72865e11a4a
omotomiwa26 <omotomiwa26@gmail.com>
2024-11-21 13:31:02 +0100

    average_price

---
+++ assignments/week_11/day_4/more_advanced_query.sql
@@ -0,0 +1,15 @@
+--Exercise 1: Aggregation with GROUP BY
+--Write a query to find the average price of books in each genre.
+select title, genre, avg(price) as average_price_of_books from books
+group by genre, title
+
+--Exercise 2: Filtering with HAVING
+--Write a query to find all authors who have written more than one book.
+
+
+--Exercise 3: Sorting Results
+--Retrieve all books and sort them alphabetically by title.
+
+
+--Exercise 4: Combining Clauses
+--Write a query to retrieve the top 2 genres by total sales, with total sales • greater than 50.
